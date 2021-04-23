3fec1694c346343ef64cdc5e491146a6f0280186
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-04-23 10:09:08 +0200

    Update eu_ES, thanks to bixerdo

---
+++ language/eu_ES/theme.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Autohide the bar of the slideshow'] = 'Automatikoki ezkutatu diapositiba-aurkezpenaren barra';
+$lang['Loop the slideshow'] = 'Errepikatu diapositiba-aurkezpena';
+$lang['More Information'] = 'Informazio gehiago';
+$lang['Slideshow Options'] = 'Diapositiba-aurkezpenaren aukerak';
+$lang['Smartpocket, Configuration Page'] = 'Smartpocket, konfigurazio orria';
