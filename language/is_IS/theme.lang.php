611d9ba46c8a79c71ee49804c389d6ec4a258ae3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-05-31 07:25:21 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/theme.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Autohide the bar of the slideshow'] = 'Fela sjálfkrafa stikuna á skyggnusýningunni';
+$lang['Loop the slideshow'] = 'Settu skyggnusýninguna í lykkju';
+$lang['More Information'] = 'Frekari upplýsingar';
+$lang['Slideshow Options'] = 'Valmöguleikar skyggnusýningar';
+$lang['Smartpocket, Configuration Page'] = 'Smartpocket, uppsetningarsíða';
