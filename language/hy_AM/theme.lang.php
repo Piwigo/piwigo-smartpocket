f520e0133b7579e8f55a7ab4ff49105f5de10675
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-01-19 15:02:54 +0100

    Update hy_AM, thanks to gayane

---
+++ language/hy_AM/theme.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Autohide the bar of the slideshow'] = 'Ինքնաթաքցնել սլայդ շոուի բարը';
+$lang['Loop the slideshow'] = 'Պտտել սլայդ շոուն';
+$lang['More Information'] = 'Լրացուցիչ տեղեկություն';
+$lang['Slideshow Options'] = 'Սլայդ շոուի ընտրանքներ';
+$lang['Smartpocket, Configuration Page'] = 'Smartpocket, կազմաձևման էջ';
