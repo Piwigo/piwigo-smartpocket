1902af992c87c06b74ffd3194b2a25bc3eed1c68
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-03 19:37:32 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/theme.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Autohide the bar of the slideshow'] = '슬라이드쇼 바 자동 숨기기';
+$lang['Loop the slideshow'] = '슬라이드쇼 반복 재생';
+$lang['More Information'] = '추가 정보';
+$lang['Slideshow Options'] = '슬라이드쇼 옵션';
+$lang['Smartpocket, Configuration Page'] = 'Smartpocket, 환경 설정 페이지';
